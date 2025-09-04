b64eac50aaa6c7c6d1441d1fa2e94f83062afb0f
Parker <parkerapplegate720@gmail.com>
2025-09-04 12:44:30 +0000

    classwork week 2

---
+++ chapters/01_1_lesson/hello_world.py
@@ -0,0 +1,9 @@
+print("Hello World!")
+
+print(1+2)
+
+print(type('1'+'2'))
+
+print('mr beast')
+
+1+66
---
+++ chapters/03_1_lesson/play.py
@@ -0,0 +1,37 @@
+def is_it_even(n):
+    if n % 2 == 0:
+        print(str(input) + ' is even')
+    else:
+        print(str(input) + ' is odd')
+
+# is_it_even(4)
+# is_it_even(5) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+x = 5
+y = 7
+
+def compare(x, y)
+   if x < y:
+       print(str(x) + ' is less than ' + str(y))
+    elif x > y:
+       print(str(x) + ' is greater than ' + str(y))
+    else:
+        print(str(x) + 'must equal' )
