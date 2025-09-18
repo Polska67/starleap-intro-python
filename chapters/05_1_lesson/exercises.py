68b48447291be29c016158994a3e35331e670a45
Parker <parkerapplegate720@gmail.com>
2025-09-18 13:58:11 +0000

    jjjj

--- chapters/05_1_lesson/exercises.py
+++ chapters/05_1_lesson/exercises.py
@@ -4,7 +4,25 @@
 
 print("********** Ch 5 Exercise 1 **********")
 
-# Do your work for Exercise 1 here
+def time_since_epoch():
+    import time 
+    seconds_per_day = 60 * 60 * 24
+    seconds_per_hour = 60 * 60
+    seconds_per_minute = 60
+
+    t = time.time()
+    print("t = ", t)
+    days = int(t / seconds_per_day)
+    print("days = ", days)
+    remainder = t % (days * 60 * 60 * 24)
+    print("remainder = ", remainder)
+    hours = int(remainder / seconds_per_hour)
+    print("hours = ", hours)
+
+
+
+time_since_epoch()
+
 
 print("Ch 5 Exercise 1: Not implemented") # Delete this line when you write your code!
 
