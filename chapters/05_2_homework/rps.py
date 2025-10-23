2b6e625de87197dcf3aa51024e58364adcd7db5d
Polska67 <parkerapplegate720@gmail.com>
2025-10-23 09:55:09 -0400

    week 10 classwork

---
+++ chapters/05_2_homework/rps.py
@@ -0,0 +1,78 @@
+Play = input('rock paper scissors three rounds: enter start to play ')
+
+
+print("Rock Paper Scissors 25")
+
+P1 = input('Player 1: ')
+P2 = input('Player 2: ')
+if P1 == "rock" and P2 == "rock":
+    print("Invalid Argument: Both Players entered same input, please try again.")
+elif P1 == "rock" and P2 == "paper":
+    print("Player 2 wins")
+elif P1 == "rock" and P2 == "scissors":
+    print == ("Player 1 wins")
+elif P1 == "paper" and P2 == "rock":
+    print == ("Player 1 wins")
+elif P1 == "paper" and P2 == "paper":
+    print("Invalid Argument: Both Players entered same input, please try again.")
+elif P1 == "paper" and P2 == "scissors":
+    print("Player 2 wins")
+elif P1 == "scissors" and P2 == "rock":
+    print("Player 2 wins")
+elif P1 == "scissors" and P2 == "paper":
+    print("Player 1 wins")
+elif P1 == "scissors" and P2 == "scissors":
+    print("Invalid Argument: Both Players entered same input, please try again.")
+else: print("invalid argument: please enter rock, paper, or scissors.")
+
+P1 = input('Player 1: ')
+P2 = input('Player 2: ')
+if P1 == "rock" and P2 == "rock":
+    print("Invalid Argument: Both Players entered same input, please try again.")
+elif P1 == "rock" and P2 == "paper":
+    print("Player 2 wins")
+elif P1 == "rock" and P2 == "scissors":
+    print == ("Player 1 wins")
+elif P1 == "paper" and P2 == "rock":
+    print == ("Player 1 wins")
+elif P1 == "paper" and P2 == "paper":
+    print("Invalid Argument: Both Players entered same input, please try again.")
+elif P1 == "paper" and P2 == "scissors":
+    print("Player 2 wins")
+elif P1 == "scissors" and P2 == "rock":
+    print("Player 2 wins")
+elif P1 == "scissors" and P2 == "paper":
+    print("Player 1 wins")
+elif P1 == "scissors" and P2 == "scissors":
+    print("Invalid Argument: Both Players entered same input, please try again.")
+else: print("invalid argument: please enter rock, paper, or scissors.")
+
+P1 = input('Player 1: ')
+P2 = input('Player 2: ')
+if P1 == "rock" and P2 == "rock":
+    print("Invalid Argument: Both Players entered same input, please try again.")
+elif P1 == "rock" and P2 == "paper":
+    print("Player 2 wins")
+elif P1 == "rock" and P2 == "scissors":
+    print == ("Player 1 wins")
+elif P1 == "paper" and P2 == "rock":
+    print == ("Player 1 wins")
+elif P1 == "paper" and P2 == "paper":
+    print("Invalid Argument: Both Players entered same input, please try again.")
+elif P1 == "paper" and P2 == "scissors":
+    print("Player 2 wins")
+elif P1 == "scissors" and P2 == "rock":
+    print("Player 2 wins")
+elif P1 == "scissors" and P2 == "paper":
+    print("Player 1 wins")
+elif P1 == "scissors" and P2 == "scissors":
+    print("Invalid Argument: Both Players entered same input, please try again.")
+else: print("invalid argument: please enter rock, paper, or scissors.")
+
+
+
+
+
+
+
+ 
